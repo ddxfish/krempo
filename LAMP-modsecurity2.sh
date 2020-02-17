bfc3813ae206fc77d6583c99e9228aae1b6ffca9
ddxfish <ddxfish@gmail.com>
2020-02-16 19:13:45 -0500

    Update LAMP-modsecurity2.sh

--- LAMP-modsecurity2.sh
+++ LAMP-modsecurity2.sh
@@ -1,7 +1,6 @@
 #Install ModSecurity2 on Apache2
 #Today is 2020-2-16
 
-
 #run it as root!!!
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
@@ -25,5 +24,14 @@ mv /usr/share/modsecurity-crs /usr/share/modsecurity-crs.old
 git clone https://github.com/SpiderLabs/owasp-modsecurity-crs.git /usr/share/modsecurity-crs
 cp /usr/share/modsecurity-crs/crs-setup.conf.example /usr/share/modsecurity-crs/crs-setup.conf
 
-#Doesn't work, stopped here
-perl -0777 -i.original -pe 's#</IfModule>#\tIncludeOptional /usr/share/modsecurity-crs/*.conf\n\tIncludeOptional /usr/share/modsecurity-crs/rules/*.conf\n</IfModule>#igs' /etc/apache2/mods-enabled/security2.conf
+#Enable these new OWASP rules in security2.conf
+perl -0777 -i.original -pe 's#</IfModule>#\tIncludeOptional /usr/share/modsecurity-crs/*.conf\n\tIncludeOptional /usr/share/modsecurity-crs/rules/*.conf\n</IfModule>#igs' /etc/apache2/mods-available/security2.conf
+
+#Enable security2 mod
+a2enmod security2
+service apache2 restart
+
+
+echo Mod Security is now setup. 
+echo Try-- http://127.0.0.1/?exec=/bin/bash
+
