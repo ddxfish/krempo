56a55285c54732e518c4d446e7b986308e580f0f
ddxfish <ddxfish@gmail.com>
2020-09-08 18:12:18 -0400

    command parameters added for convenience $1

--- lamp-backups.sh
+++ lamp-backups.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #script to back up SQL and files for website
-#Version 2020.1
-#Today is: 6-2-2020
-#Ubuntu is 20.04 LTS
+#Version 2020.2
+#Today is: 9-8-2020
+#Ubuntu is 20.04.1 LTS
+#
+#params: lamp-backups.sh $websitename $databasename $databaseusername $databaseuserpassword
 
 #run it as root!!!
 if [[ $EUID -ne 0 ]]; then
@@ -22,19 +24,9 @@ cd /root/website-backups
 
 clear
 echo "We are enabling weekly backups that rotate in /root/website-backups"
-echo "You only need to do this once"
-echo Give your website backup files a friendly name without spaces
-read websitename
-echo Input the full path to your website root directory without trailing slash /
-read websitepath
-echo Input database name:
-read databasename
-echo Input database username:
-read databaseusername
-echo Input database user password
-read databaseuserpassword
+echo "You only need to run this setup once"
 echo ----------Creating backup script------------
-
+pause 3
 echo "
 rm -fr old4
 mkdir current
@@ -49,8 +41,10 @@ mv current old1
 
 mkdir current
 
-tar czf current/$websitename.tgz $websitepath/*
-mysqldump -u $databaseusername -p$databaseuserpassword $databasename > current/$databasename.sql
+websitepath = "/var/www/$1/html"
+
+tar czf current/$1.tgz $websitepath/*
+mysqldump -u $3 -p$4 $2 > current/$2.sql
 touch current/`date +%F--%T`
 " | tee /root/website-backups/backup.sh
 
