f6abb49b119010aedfa1056ea9f6fdfe0d248d0b
ddxfish <ddxfish@gmail.com>
2020-09-08 21:35:06 -0400

    auto accept apt-get install

--- lamp-letsencrypt.sh
+++ lamp-letsencrypt.sh
@@ -2,6 +2,6 @@ sudo apt-get update
 sudo apt-get install software-properties-common
 sudo add-apt-repository universe
 sudo apt-get update
-sudo apt-get install certbot python3-certbot-apache
+sudo apt-get -y install certbot python3-certbot-apache
 sudo certbot --apache
 sudo certbot renew --dry-run
