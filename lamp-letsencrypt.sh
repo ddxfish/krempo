4d2291c63a4923a11ea236b774a0351b80c89878
ddxfish <ddxfish@gmail.com>
2021-09-09 11:04:15 -0400

    pause for output
    no error handling, so added a user pause to check output

--- lamp-letsencrypt.sh
+++ lamp-letsencrypt.sh
@@ -5,3 +5,7 @@ sudo apt-get update
 sudo apt-get -y install certbot python3-certbot-apache
 sudo certbot --apache
 sudo certbot renew --dry-run
+echo Krempo: Please read the above output to make sure things worked.
+echo the script does not yet detect errors during SSL cert additions
+echo press enter to continue
+read nothing
