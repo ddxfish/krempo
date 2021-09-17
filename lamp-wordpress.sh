5de305435f6e84b93f8fb366eb5a2324044c4f3f
ddxfish <ddxfish@gmail.com>
2021-09-17 15:03:51 -0400

    automated

--- lamp-wordpress.sh
+++ lamp-wordpress.sh
@@ -4,8 +4,8 @@
 #Today is: 9-8-2021
 #Ubuntu is 20.04.3 LTS
 #
-#params: lamp-wordpress.sh $1 $2 $3 $4 $5 $6
-#params: lamp-wordpress.sh $websitename $subdomain $websitedir $databasename $databaseusername $databaseuserpassword $fulldomain
+#params: lamp-wordpress.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
+#params: lamp-wordpress.sh $websitename $subdomain $websitedir $databasename $databaseusername $databaseuserpassword $fulldomain $wordpresspass $rootpass
 #Wordpress files
 cd /var/www/$1/$2
 rm latest.tar.gz
@@ -24,9 +24,9 @@ echo 1- Create DB, 2- Create User, 3- Grant
 echo Go ahead with your root pass three times:
 
 #create database with info given
-mysql -u root -p -e "CREATE DATABASE $4"
-mysql -u root -p -e "CREATE USER '$5'@'localhost' IDENTIFIED BY '$6';"
-mysql -u root -p -D $4 -e "GRANT ALL ON $4.* TO '$5'@'localhost';"
+mysql -u root -p$9 -e "CREATE DATABASE $4"
+mysql -u root -p$9 -e "CREATE USER '$5'@'localhost' IDENTIFIED BY '$6';"
+mysql -u root -p$9 -D $4 -e "GRANT ALL ON $4.* TO '$5'@'localhost';"
 
 #install wordpress CLI tool
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
@@ -36,22 +36,12 @@ sudo mv wp-cli.phar /usr/local/bin/wp
 clear
 cd $3
 #install wordpress with random password
-wordpresspass=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1`
+
 #create config
 wp config create --allow-root --dbname=$4 --dbuser=$5 --dbpass=$6
 #create DB -- might fail due to already created, thats okay
 #wp db create --allow-root
 #echo "Krempo: Errors are okay if they say database already exists"
 #install the core
-wp core install --allow-root --url=$7 --title=$1 --admin_user=supervisor --admin_password=$wordpresspass --admin_email=info@$1
+wp core install --allow-root --url=$7 --title=$1 --admin_user=supervisor --admin_password=$8 --admin_email=info@$1
 sleep 3
-
-#MAKE SURE USER TAKES THE LOGIN INFO
-echo Copy your Wordpress login information:
-echo -----------------------------------
-echo host: http://$7/wp-admin/
-echo user: supervisor
-echo pass: $wordpresspass
-read nothing
-echo Are you really sure you got that? Its important.
-read nothing
